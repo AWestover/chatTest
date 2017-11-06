eb24ecb3c4fdb4e3417eb629f8816b721ee4d1ac
Alek Westover <alek.westover@gmail.com>
2017-11-05 19:35:53 -0500

    test

---
+++ chat/server.js
@@ -0,0 +1,37 @@
+//
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var users = [];
+var connections = [];
+
+var port = process.env.PORT || 3000;
+
+server.listen(port);
+
+console.log("server.js is running");
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+io.sockets.on('connection', function(socket){
+  connections.push(socket);
+  console.log("Connected: %s sockets connected", connections.length);
+
+  //disconect
+  socket.on('disconnect', function(data) {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("Connected: %s sockets connected", connections.length);
+  });
+
+
+  //send message
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data});
+  })
+
+});
